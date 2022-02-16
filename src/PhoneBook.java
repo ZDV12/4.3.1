2c06135bc7eebcd6f76ab405ce3da738a58c4e3c
DenisZharkih <deniszharkih@yandex.ru>
2022-02-16 15:51:03 +0700

    HashMap | Тел. справочник

---
+++ src/PhoneBook.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneBook {
+
+    private String groupName;
+    List<Contact> listContact;
+
+    Map<String,List<Contact>> p = new HashMap<String,List<Contact>>();
+}
