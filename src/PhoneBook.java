f759bb20f8b410c8f0d89fcf762b82786b49021b
DenisZharkih <deniszharkih@yandex.ru>
2022-02-17 16:09:38 +0700

    Доработка от 17.02.22. Описал методы добавления контакта (с указанием имени группы и без указания имени группы), добавление группы контактов. Необходимо доделать: - Добавление контакта в разные группы; - Поиск контактов по группе; - Поиск контакта по номеру (одну из этих двух последних операций можно сделать перебором);

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,11 +1,50 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PhoneBook {
 
-    private String groupName;
-    List<Contact> listContact;
+    public PhoneBook (String phoneBookName){
+        this.phoneBookName = phoneBookName;
+    }
 
-    Map<String,List<Contact>> p = new HashMap<String,List<Contact>>();
+    public String getPhoneBookName() {
+        return phoneBookName;
+    }
+
+    private String phoneBookName;
+    Map<String,List<Contact>> groupBase = new HashMap<String,List<Contact>>();
+
+    public void addContact (Contact contact){
+        List<Contact> defaultContactList = new ArrayList<Contact>();
+        defaultContactList.add(contact);
+        if (groupBase.containsKey("defaultGroup")){
+            groupBase.get("defaultGroup").add(contact);
+        }
+        else{
+            groupBase.put("defaultGroup", defaultContactList );
+            groupBase.get("defaultGroup").add(contact);
+        }
+    }
+
+    public void addContact (Contact contact, String groupName){
+        List<Contact> contactList = new ArrayList<Contact>();
+        if (groupBase.containsKey(groupName)){
+            groupBase.get(groupName).add(contact);
+        }
+        else{
+            groupBase.put(groupName, contactList);
+            groupBase.get(groupName).add(contact);
+        }
+    }
+
+    public void addGroup (String groupName, ArrayList<Contact> contactList){
+        groupBase.put(groupName, contactList);
+    }
+
+    public void addGroup (String groupName){
+        List<Contact> contactList = new ArrayList<Contact>();
+        groupBase.put(groupName, contactList);
+    }
 }
